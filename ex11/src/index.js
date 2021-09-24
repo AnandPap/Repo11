1bc17a54035d379ad1c6f2abbf43fb05475ce7f2
Anand Pap <anandpap@live.com>
2021-09-24 09:08:40 +0200

    Splitting array

---
+++ ex11/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+function splitArrayInGroups(arr, n) {
+    var result = [];
+    var i;
+    var k = n;
+    for (i = 0; i < arr.length; i++) {
+       if (k > 0) {
+           if (i % n == 0) {
+                result[n - k] = arr.slice(i, i + n);
+                k--;
+           }
+       } 
+    }
+    return result;
+}
+// Only change code above this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2)); // Change this line
+module.exports = splitArrayInGroups;
