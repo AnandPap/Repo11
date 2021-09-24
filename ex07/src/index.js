a0d0db7bcaf869814f923a707b9fbaa55e138030
Anand Pap <anandpap@live.com>
2021-09-24 10:19:58 +0200

    Returning the largest numbers

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -17,5 +17,5 @@ function largestNumFromArr(arr) {
     return maxNumArray;
 }
 //Only change code above this line
-console.log(largestNumFromArr());   // Change this line
+console.log(largestNumFromArr([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));   // Change this line
 module.exports = largestNumFromArr;
