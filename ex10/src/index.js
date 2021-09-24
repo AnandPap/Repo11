36e249de86659e4d0e641d27fa65df3d3964af6f
Anand Pap <anandpap@live.com>
2021-09-24 08:00:28 +0200

    Splice

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,6 +1,10 @@
 // Only change code below this line
 function mySplice(arr1, arr2, n) {
-    
+    var i;
+    for (i = 0; i < arr1.length; i++) {
+        arr2.splice(n, 0, arr1[i]);
+    }
+    return arr2;
 }
 // Only change code above this line
 console.log(mySplice()); // Change this line
